1572fd6b2c1182aa0fcebe57b70bb008f9e8c13c
Sakshi <ps1228103@gmail.com>
2024-07-03 15:03:23 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const { db } = require('./db/db')
+const app = express()
+require('dotenv').config()
+const {readdirSync} = require('fs')
+
+const PORT = process.env.PORT
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+const server = () =>{
+    db()
+   app.listen(PORT, ()=>[
+     console.log("listening:", PORT)
+   ])
+}
+
+server()
+
+//72GdpiSj2xWwBg2A
---
+++ backend/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const db = async () => {
+    try {
+        mongoose.set('strictQuery', false)
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log('Db Connected')
+    } catch (error) {
+        console.log('DB Connection Error');
+    }
+}
+
+module.exports = {db}
