eba8b65a176e3cf4dee19343a49bf22d52045ddf
sakshisingh79 <114875296+sakshisingh79@users.noreply.github.com>
2024-07-03 15:11:59 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,13 @@ const PORT = process.env.PORT
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+          origin:["https//deploy-mern-1whq.vercel.app"],
+          methods: ["POST", "GET"],
+          credentials: true
+      }
+            ))
 
 
 //routes
